1dd553cc7960b78508c6ec97524459b6eec81412
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-15 20:08:52 +0200

    strike

--- todo.js
+++ todo.js
@@ -1,4 +1,5 @@
 var submitB = document.getElementById("submitButton");
+var striked=false;
 
 submitB.addEventListener("click", addLi);
 
@@ -9,9 +10,29 @@ function addLi() {
   var newLi = document.createElement("li");
   var newText = document.createTextNode(input);
 
+  
   newButton.appendChild(newText);
+  newButton.addEventListener("click",function(){
+    striked=!striked;
+    switch(striked){
+      case true:
+      newButton.style.textDecoration="line-through";
+      break;
+      case false:
+      newButton.style.textDecoration="none";
+      break;
+    }
+    
+    
+  });
   newButton.setAttribute("type", "button");
+  
+  
   newLi.appendChild(newButton);
   ulList.appendChild(newLi);
   var reset = document.getElementById("myForm").reset();
+}
+
+function strikeText(){
+  
 }
