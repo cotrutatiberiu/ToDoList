ac2d9ce3bfacb90e65d9298975a5eeac21a4ac12
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-15 23:41:04 +0200

    test

--- todo.js
+++ todo.js
@@ -1,38 +1,38 @@
 var submitB = document.getElementById("submitButton");
-var striked=false;
 
 submitB.addEventListener("click", addLi);
 
 function addLi() {
+  var striked = false;
   var input = document.getElementById("inputId").value;
   var newButton = document.createElement("button");
   var ulList = document.getElementById("list");
   var newLi = document.createElement("li");
   var newText = document.createTextNode(input);
 
-  
   newButton.appendChild(newText);
-  newButton.addEventListener("click",function(){
-    striked=!striked;
-    switch(striked){
-      case true:
-      newButton.style.textDecoration="line-through";
-      break;
-      case false:
-      newButton.style.textDecoration="none";
-      break;
-    }
-    
-    
-  });
+  newButton.addEventListener("click", deleteElem);
   newButton.setAttribute("type", "button");
-  
-  
+  newButton.style.width="387px";
+
   newLi.appendChild(newButton);
   ulList.appendChild(newLi);
   var reset = document.getElementById("myForm").reset();
-}
 
-function strikeText(){
-  
+
+  // function strikeText() {
+  //   striked = !striked;
+  //   switch (striked) {
+  //     case true:
+  //       newButton.style.textDecoration = "line-through";
+  //       break;
+  //     case false:
+  //       newButton.style.textDecoration = "none";
+  //       break;
+  //   }
+  // }
+
+  function deleteElem(){
+    ulList.removeChild(ulList.childNodes);
+  }
 }
