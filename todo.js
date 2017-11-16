214f457e80ed7d423f75068d68157d343e8cfacf
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-16 10:47:13 +0200

    delete button

--- todo.js
+++ todo.js
@@ -6,33 +6,44 @@ function addLi() {
   var striked = false;
   var input = document.getElementById("inputId").value;
   var newButton = document.createElement("button");
+  var deleteLiButton = document.createElement("button");
   var ulList = document.getElementById("list");
   var newLi = document.createElement("li");
-  var newText = document.createTextNode(input);
-
-  newButton.appendChild(newText);
-  newButton.addEventListener("click", deleteElem);
-  newButton.setAttribute("type", "button");
-  newButton.style.width="387px";
-
-  newLi.appendChild(newButton);
-  ulList.appendChild(newLi);
   var reset = document.getElementById("myForm").reset();
 
+  var newText = document.createTextNode(input);
+  var deleteText = document.createTextNode("x");
 
-  // function strikeText() {
-  //   striked = !striked;
-  //   switch (striked) {
-  //     case true:
-  //       newButton.style.textDecoration = "line-through";
-  //       break;
-  //     case false:
-  //       newButton.style.textDecoration = "none";
-  //       break;
-  //   }
-  // }
+  newButton.appendChild(newText);
+  deleteLiButton.appendChild(deleteText);
 
-  function deleteElem(){
-    ulList.removeChild(ulList.childNodes);
+  newButton.addEventListener("click", strikeText);
+  newButton.setAttribute("type", "button");
+  newButton.style.width = "387px";
+
+  deleteLiButton.addEventListener("click", deleteLi);
+  deleteLiButton.setAttribute("type", "button");
+
+  newLi.appendChild(newButton);
+  newLi.appendChild(deleteLiButton);
+
+  ulList.appendChild(newLi);
+
+  //Function click on element to strike
+  function strikeText() {
+    striked = !striked;
+    switch (striked) {
+      case true:
+        newButton.style.textDecoration = "line-through";
+        break;
+      case false:
+        newButton.style.textDecoration = "none";
+        break;
+    }
+  }
+
+  //Function to remove a list element
+  function deleteLi() {
+    ulList.removeChild(newLi);
   }
 }
